16351c8c8f98ce60ecdcc1d7307f1a1ce3205bc8
Семён Денисов <69531676+Samaelish@users.noreply.github.com>
2020-11-04 16:12:22 +0300

    done

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgSlider(any) {
+    document.querySelector('.starbucks').src = any;
+}
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
